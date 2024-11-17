d6233f7507292182ab18884e99a7aade9e2289c0
Manhz <ndmanh162@gmail.com>
2024-11-17 11:13:49 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -2,33 +2,40 @@
 include "../model/pdo.php";
 include "header.php";
 
-if(isset($_GET['act'])){
-    $act =$_GET['act'];
-    switch ($act){
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
+    switch ($act) {
         case 'adddm':
-            if(isset($_POST['themmoi'])&&($_POST['themmoi'])){
-                $tensp = $_POST['tensp'];
-                $sql = "insert into categories(name) values ('$tensp')";
-                pdo_execute($sql);
+            if (isset($_POST['themmoi']) && ($_POST['themmoi'])) {
+                $tenloai = $_POST['tenloai'];
+                $sql = "INSERT INTO danhmuc(name) VALUES(?)";
+                pdo_execute($sql, [$tenloai]);
                 $thongbao = "Thêm thành công";
             }
             include "danhmuc/add.php";
             break;
-        case 'addsp':
-            include "sanpham/add.php";
+
+        case 'lisdm':
+            $sql = "SELECT * FROM danhmuc ORDER BY name";
+            $listdanhmuc = pdo_query($sql);
+            include "danhmuc/list.php";
+            break;
+
+        case 'xoadm':
+            if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+                $sql = "DELETE FROM danhmuc WHERE id = ?";
+                pdo_execute($sql, [$_GET['id']]);
+                $thongbao = "Xóa thành công";
+            } else {
+                $thongbao = "Xóa thất bại: ID không hợp lệ";
+            }
+            include "danhmuc/list.php";
             break;
 
         default:
             include "home.php";
             break;
     }
-
-    }else {
-        include "home.php";
-    }
-
-include "footer.php";
-
-
-
-?>
+} else {
+    include "home.php";
+}
