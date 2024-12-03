3d6c9072f91f6fbadae805f7074d91defc9bb143
Cubin23 <hungabc8511@gmail.com>
2024-12-04 06:47:49 +0700

    init project

--- 3.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-session_start();
-session_destroy();
-echo '<h1>Session đã hủy</h1>';
-echo '<a href="1.php">Khởi tạo</a>';
-echo '<a href="2.php">Show session</a>';
-
-?>
--- global.php
+++
@@ -1,3 +0,0 @@
-<?php
-$img_path= "DA1/upload/" 
-?>
